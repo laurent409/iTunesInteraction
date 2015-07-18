967c08feba7db55e84de10c2c6045ee2b1a6f43e
Laurent Philibert <philibert.laurent@gmail.com>
2015-07-18 22:20:33 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# iTunesInteraction
--- itunesinteraction/ViewController.swift
+++ itunesinteraction/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableViewViewController: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +20,18 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        cell.textLabel!.text = "Ligne #\(indexPath.row)"
+        cell.detailTextLabel?.text = "Subtitles #\(indexPath.row)"
+        
+        return cell
     }
 
 

1c688b489f15f12a93e46cf8795ae4154786a52b
Laurent Philibert <philibert.laurent@gmail.com>
2015-07-22 22:22:12 +0200

    Delete APIController.swift

--- itunesinteraction/APIController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  APIController.swift
-//  itunesinteraction
-//
-//  Created by Laurent Philibert on 19/07/2015.
-//  Copyright (c) 2015 Laurent Philibert. All rights reserved.
-//
-
-import Foundation
